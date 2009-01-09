2a59a4d0c496bbf7bbb4c948b666debb9089ca85
Christopher Groskopf <cgroskop@calpoly.edu>
2009-01-09 09:50:00 -0800

    Renamed to SaneMe (Scanner Access Now Even More Easy).

--- sane/errors.py
+++ sane/errors.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 
-#~ This file is part of Pysapi.
+#~ This file is part of SaneMe.
 
-#~ Pysapi is free software: you can redistribute it and/or modify
+#~ SaneMe is free software: you can redistribute it and/or modify
 #~ it under the terms of the GNU General Public License as published by
 #~ the Free Software Foundation, either version 3 of the License, or
 #~ (at your option) any later version.
 
-#~ Pysapi is distributed in the hope that it will be useful,
+#~ SaneMe is distributed in the hope that it will be useful,
 #~ but WITHOUT ANY WARRANTY; without even the implied warranty of
 #~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #~ GNU General Public License for more details.
 
 #~ You should have received a copy of the GNU General Public License
-#~ along with Pysapi.  If not, see <http://www.gnu.org/licenses/>.
+#~ along with SaneMe.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 This module contains the Python exceptions which take the place
