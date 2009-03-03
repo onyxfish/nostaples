c444aa42180805d510d0e40fea5a36879bcea5ca
Christopher Groskopf <staringmonkey@gmail.com>
2009-03-02 20:59:24 -0800

    Updated setup file.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name='nostaples',
-      version='0.2.0',
+      version='0.3.0',
       description='GNOME Document Scanning Application',
       author='Christopher Groskopf',
       author_email='staringmonkey@gmail.com',
       url='http://www.etlafins.com/nostaples',
       license='GPL',
-      packages=['nostaples', 'nostaples.controllers', 'nostaples.models', 'nostaples.utils', 'nostaples.views', 'nostaples.unittests'],
+      packages=['nostaples', 'nostaples.controllers', 'nostaples.models', 'nostaples.utils', 'nostaples.views', 'nostaples.unittests', 'nostaples.sane'],
       package_dir={'nostaples' : ''},
       package_data={'nostaples' : ['logging.config', 'gui/*.glade']},
       scripts = ['nostaples'],
